0116e0ce6f08d551190f46ea991b65135b7d7c47
Luis Perez <luisperez07182@email.com>
2024-02-08 22:06:16 -0600

    keep going with limited guesses

--- guess_number.py
+++ guess_number.py
@@ -1,12 +1,17 @@
 number = 10
+guesses = 5
+count = 0
 
 print("I'm thinking of a number...")
 
-while True:
-    guess = input("What number am I thinking of or Enter q to quit? ")
+while count < guesses:
+    count += 1
+    tries_left = guesses - count
+    guess = input(f"What number am I thinking of or Enter q to quit?  You have {tries_left + 1} attempts left.\n")
 
     if guess.lower() == 'q':
         print(f'Sorry! The number was {number}')
+        break
 
     guess = int(guess)
 
@@ -15,3 +20,6 @@ while True:
         break
     else:
         print(f'Sorry! Thats not the correct number. Retry!')
+
+if count == guesses:
+    print(f'You have used up all your attempts.')
